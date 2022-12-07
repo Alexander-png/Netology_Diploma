9a2f5337aa8fe31e4c494c41a4d27c04b6961907
Alexander <sanek41km@mail.ru>
2022-12-07 23:39:50 +0300

    Added assets for stone trap location

--- Assets/Platformer3d/Scripts/LevelEnvironment/Elements/Common/Platform.cs
+++ Assets/Platformer3d/Scripts/LevelEnvironment/Elements/Common/Platform.cs
@@ -8,5 +8,19 @@ namespace Platformer3d.LevelEnvironment.Elements.Common
 		private bool _climbable;
 
 		public bool Climbable => _climbable;
-	}
+
+        private void OnDrawGizmos()
+        {
+            Color c = Color.green;
+            c.a = 0.6f;
+            Gizmos.color = c;
+
+            var collider = GetComponent<BoxCollider>();
+            if (collider != null)
+            {
+
+                Gizmos.DrawCube(transform.position, collider.size);
+            }
+        }
+    }
 }
